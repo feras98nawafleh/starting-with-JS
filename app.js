587494ae862707139febbbe816deddfd44e87f40
feras98nawafleh <>
2021-06-17 10:55:11 +0000

    added a little css and JS

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
-var GOAT = window.prompt("Who's your greatest player of all time? you can answer with name or shirt number");
-if(GOAT == "messi" || GOAT == 10) {
+var GOAT = window.prompt("Who's your greatest player of all time?");
+
+while(GOAT.toLowerCase() != "messi" && GOAT.toLowerCase() != "ronaldo" && GOAT.toLowerCase() != "kaka") {
+  window.alert("WRONG ENTRY! no such player, please try again...");
+  var GOAT = window.prompt("Who's your greatest player of all time?");
+}
+
+if(GOAT.toLowerCase() == "messi") {
   alert("MESSI!! you're deffinitely right");
   document.write(`<div class="messi">
-        <h2>Leonel Messi</h2>
+        <h2>Lionel Messi</h2>
         <img src="https://specials-images.forbesimg.com/imageserve/5f15af31465263000625ce08/960x0.jpg?fit=scale" alt="">
       </div>`)
-} else if(GOAT == "ronaldo" || GOAT == 7) {
+} else if(GOAT.toLowerCase() == "ronaldo") {
   alert("Good Choice!");
   document.write(`<div class="ronaldo">
         <h2>C. Ronaldo</h2>
         <img src="https://sport360.com/wp-content/uploads/2017/12/CR7winner.jpg" alt="">
       </div>`)
-} else if(GOAT == "kaka" || GOAT == 8){
+} else if(GOAT.toLowerCase() == "kaka"){
   alert("KAKA! what a player");
   document.write(`<div class="kaka">
         <h2>Kaka</h2>
-        <img src="https://preview.redd.it/ej5fepfa8z101.jpg?width=640&crop=smart&auto=webp&s=7dc88d3a668d1a9666964f7bbfbb53713330c14b" alt="">
+        <img src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2017/12/17/15135288171302.jpg" alt="">
       </div>`)
-} else {
-  alert("Wrong entry!");
-  document.write(`<div>
-  <h2>NO SUCH OPTION</h2>
-      </div>`)
+}
+
+var Rate = window.prompt("on a scale from 1-5, how would you rate "+ GOAT.toLowerCase() + "?");
+
+while(Rate < 1 || Rate > 5) {
+  window.alert("please stick to rate from 1 to 5");
+  var Rate = window.prompt("on a scale from 1-10, how would you rate "+ GOAT.toLowerCase() + "?");
+}
+
+for(var i = 0;i<Rate;i++) {
+  document.write(`<i class="fas fa-star"></i>
+`)
 }
 
 
